2bc67897c25058d862e26ced53bd341d8a74db60
Nathan Jones <scholyx@gmail.com>
2019-01-28 12:40:09 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -3,3 +3,9 @@ df = df.replace({r'\s+$': '', r'^\s+': ''}, regex=True).replace(r'\n',  ' ', reg
 
 #Filter out different criteria like: Type = S, iQ is not Nan, and rows/cells that are all uppercase
 new_df = df[(df.Type == 'S') & (df.Product.isna()) & (df.QUESTION.str.isupper())]
+
+#Drop any rows that contain any/a single empty/NaN value (axis = 1 for columns)
+df = df.dropna(axis=0, how='any')
+
+#Drop any rows that are all empty/NaN values (axis = 1 for columns)
+df = df.dropna(axis=0, how='all')
