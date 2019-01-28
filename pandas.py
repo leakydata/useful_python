662a32efe6a47728692c80dacf76f0dbeb2bf222
Nathan Jones <scholyx@gmail.com>
2019-01-28 13:23:43 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -9,3 +9,10 @@ df = df.dropna(axis=0, how='any')
 
 #Drop any rows that are all empty/NaN values (axis = 1 for columns)
 df = df.dropna(axis=0, how='all')
+
+# Create new row and set value based on value in other column or series
+df['answer_type'] = np.where((df['numeric_answer'] != 'nan'), 1, 0)
+
+#Find rows that contain a string or part of string and set value of new row to 1
+df['reps_only'] = np.where((df['question'].str.contains("Rep's ONLY", flags=re.IGNORECASE, regex=True)), 'rep', 0)
+
