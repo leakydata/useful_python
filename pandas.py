a789a95a73eb0db7607c767b86bbcf5ee4e5a756
Nathan Jones <scholyx@gmail.com>
2019-01-28 12:37:48 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,2 +1,5 @@
 #Remove new line (\n) characters from the entire dataframe
 df = df.replace({r'\s+$': '', r'^\s+': ''}, regex=True).replace(r'\n',  ' ', regex=True)
+
+#Filter out different criteria like: Type = S, iQ is not Nan, and rows/cells that are all uppercase
+new_df = df[(df.Type == 'S') & (df.Product.isna()) & (df.QUESTION.str.isupper())]
