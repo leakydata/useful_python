e8b7aeb0c58aec299106243b98c233cb0fdf4cea
Nathan Jones <scholyx@gmail.com>
2019-03-01 17:25:47 -0500

    Update pptx.py

--- pptx.py
+++ pptx.py
@@ -67,38 +67,66 @@ prs.slides[40].shapes[1].chart.chart_type._member_name # COLUMN_STACKED
 prs.slides[40].shapes[1].chart.chart_type.real # 52
 prs.slides[40].shapes[1].chart.chart_type._docstring # Stacked Column.
 
-# Pull all the values out of charts and convert them into lists of dicts
+# Pull all the values out of charts and convert them into dicts then dataframes
 from collections import defaultdict
 for slide_idx,slide in enumerate(prs.slides):
     for i,shape in enumerate(slide.shapes):
         chart_data = defaultdict(list) 
+        
         if shape.shape_type == MSO_SHAPE_TYPE.CHART: # THIS IS WHERE THE CHART SHAPE STARTS (All other shapes are excluded for now)
+            
             ch = shape.chart 
             ch_labels = x_labels(ch)
             ch_series = chart_vals(ch)
             chart_name = shape.name.upper()
-
+            
+            print("\n\n----------------- SLIDE:",slide_idx+1,"SHAPE:",i, chart_name,"------------------------------")
+            
             if len(ch_series) > 0:
-                
+                empty_column_indexes = []
+                for idx,l in enumerate(ch_series):
+                    if all(v is None for v in l):
+                        empty_column_indexes.append(idx)
+
                 for s in range(len(ch_series)):
                     for key, value in zip(ch_labels,ch_series[s]):
                         chart_data[key].append(value)
-                        
-                if '' in chart_data:
-                        del chart_data['']
                 
+                if '' in chart_data:
+                    del chart_data['']
+                    
                 df = pd.DataFrame.from_dict(chart_data)
                 df.dropna(axis=0,how='all',inplace=True) 
                 final_chart_data = list(df.T.to_dict().values())
                 
+                legend = []
                 if ch.has_legend:
                     legend = get_legend(ch)
-                    if len(get_legend(ch)) == 1 and '' in legend:
-                        final_chart_data = dict(zip(legend,final_chart_data))
-                if bool(final_chart_data):        
-                    print("SLIDE#:",slide_idx+1, "FINAL DATA:", final_chart_data)
-                else:
-                    print("SLIDE#:",slide_idx+1, "FINAL DATA:", "-- EMPTY CHART --")
+                    if bool(empty_column_indexes):
+                        for del_col_idx in empty_column_indexes:
+                            del legend[del_col_idx]
+                    final_chart_data = dict(zip(legend,final_chart_data))
+                    
+                else:        
+                    if bool(final_chart_data):
+                        if bool(ch.plots[0].series[0]._ser.xpath('//c:tx//c:v/text()')):
+                            legend = list(ch.plots[0].series[0]._ser.xpath('//c:tx//c:v/text()'))
+                            if bool(empty_column_indexes):
+                                for del_col_idx in empty_column_indexes:
+                                    del legend[del_col_idx]
+                            final_chart_data = dict(zip(legend, final_chart_data))
+
+                        if type(final_chart_data) is list:
+                            final_chart_data = final_chart_data[0]
+                   
+                    if dict_levels(final_chart_data) > 1:
+                        print(pd.DataFrame.from_dict(final_chart_data))
+                    else:
+                        ind = [str(x) for x in range(len(final_chart_data))]
+                        final_chart_data = dict(zip(ind, [final_chart_data]))
+                        print(pd.DataFrame.from_dict(final_chart_data))
+            else:
+                print("SLIDE#:",slide_idx+1, "FINAL DATA:", "-- EMPTY CHART --")
 
 
 #------- Smart Shapes i.e. Diagrams ----------
