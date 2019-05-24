711f52e22d2edda6ee533ba7bbc5658ba27b0059
Nathan Jones <scholyx@gmail.com>
2019-05-24 18:06:37 -0400

    Update general.py

--- general.py
+++ general.py
@@ -14,3 +14,9 @@ clean_dict = filter(lambda k: not isnan(my_dict[k]), my_dict)
 # dict comprehension
 clean_dict = {k: my_dict[k] for k in my_dict if not isnan(my_dict[k])}
 
+# Remove NaNs in a dict
+import simplejson
+
+clean_dict  = simplejson.loads(simplejson.dumps(my_dict, ignore_nan=True))
+## or depending on your needs
+clean_dict  = simplejson.loads(simplejson.dumps(my_dict, allow_nan=False))
