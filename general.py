80f15c79a4110fc731aaa159c4f1133c5b98f1df
Nathan Jones <scholyx@gmail.com>
2019-05-24 16:12:09 -0400

    Update general.py

--- general.py
+++ general.py
@@ -1,6 +1,16 @@
-# Remove NaNs from a dict
+from math import isnan
+
+##if nans are being stored as keys:
+# functional
+clean_dict = filter(lambda k: not isnan(k), my_dict)
+
+# dict comprehension
+clean_dict = {k: my_dict[k] for k in my_dict if not isnan(k)}
+
+##if nans are being stored as values:
 # functional
 clean_dict = filter(lambda k: not isnan(my_dict[k]), my_dict)
 
 # dict comprehension
 clean_dict = {k: my_dict[k] for k in my_dict if not isnan(my_dict[k])}
+
