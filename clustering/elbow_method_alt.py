7cf6aee582f1e0add40abae50156b0bd56584368
Nathan Jones <scholyx@gmail.com>
2022-02-16 13:31:19 -0500

    Create elbow_method_alt.py

---
+++ clustering/elbow_method_alt.py
@@ -0,0 +1,24 @@
+#https://jtemporal.com/kmeans-and-elbow-method/
+def calculate_wcss(data):
+    wcss = []
+    for n in range(2, 21):
+        kmeans = KMeans(n_clusters=n)
+        kmeans.fit(X=data)
+        wcss.append(kmeans.inertia_)
+
+    return wcss
+
+  
+def optimal_number_of_clusters(wcss):
+    x1, y1 = 2, wcss[0]
+    x2, y2 = 20, wcss[len(wcss)-1]
+
+    distances = []
+    for i in range(len(wcss)):
+        x0 = i+2
+        y0 = wcss[i]
+        numerator = abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1)
+        denominator = sqrt((y2 - y1)**2 + (x2 - x1)**2)
+        distances.append(numerator/denominator)
+    
+    return distances.index(max(distances)) + 2
