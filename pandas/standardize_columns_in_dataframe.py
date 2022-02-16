85c5770236a0676acb4dd0c3d579d1df85117c65
Nathan Jones <scholyx@gmail.com>
2022-02-16 09:12:41 -0500

    Update standardize_columns_in_dataframe.py

--- pandas/standardize_columns_in_dataframe.py
+++ pandas/standardize_columns_in_dataframe.py
@@ -2,3 +2,6 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 df[[x for x in df.columns.tolist()]] = StandardScaler().fit_transform(df[[x for x in df.columns.tolist()]])
+
+#Create a new numerical data only dataframe
+df_std = pd.DataFrame(df, columns = [x for x in df.columns.tolist()])
